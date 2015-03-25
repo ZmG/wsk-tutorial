be84a47faa933f8bb9922b9d0a1923e85af5ec72
Sanjay Joshi <joshisa@us.ibm.com>
2015-03-25 10:22:44 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name='docker-tutorial',
+    name='trycedocker-tutorial',
     version='0.2.1',
-    author=u'Thatcher Peskens',
-    author_email='thatcher@dotcloud.com',
+    author=u'IBM jStart',
+    author_email='jstart@us.ibm.com',
     packages=find_packages(),
-    url='https://github.com/dhrp/docker-tutorial/',
-    license='To be determined',
-    description='An interactive learning environment to get familiar with the Docker cli',
+    url='https://hub.jazz.net/git/joshisa/trycedocker-tutorial/',
+    license='TBD',
+    description='An interactive learning environment to get familiar with the IBM Containers Extension cli',
     long_description=open('README.md').read(),
     include_package_data=True,
 )
