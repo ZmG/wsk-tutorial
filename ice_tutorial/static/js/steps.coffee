823508278b78df300a216215ea1a2c65d1172b9a
Miguel Clement <mfclemen@us.ibm.com>
2015-05-01 13:52:14 -0400

    .

--- ice_tutorial/static/js/steps.coffee
+++ ice_tutorial/static/js/steps.coffee
@@ -982,13 +982,6 @@ window.switchToAdvanced = switchToAdvanced = () ->
   tutorialTop.animate({ backgroundColor: "#3F2626" }, 1000 )
   advancedTag.fadeIn()
 
-  marker = statusMarker.clone()
-  marker.prependTo(progressIndicator)
-
-  marker.attr("id", "marker-" + i)
-  marker.find('text').get(0).textContent = '←'
-  marker.click( -> next(0) )
-
   questionNumber = 0
   for question in adv_q
     f = buildfunction(question)
--- ice_tutorial/static/js/steps.js
+++ ice_tutorial/static/js/steps.js
@@ -613,7 +613,7 @@
   };
 
   window.switchToAdvanced = switchToAdvanced = function() {
-    var f, j, len, marker, question, questionNumber;
+    var f, j, len, question, questionNumber;
     questions = [];
     statusMarker.nextAll('span').remove();
     leftside.animate({
@@ -623,13 +623,6 @@
       backgroundColor: "#3F2626"
     }, 1000);
     advancedTag.fadeIn();
-    marker = statusMarker.clone();
-    marker.prependTo(progressIndicator);
-    marker.attr("id", "marker-" + i);
-    marker.find('text').get(0).textContent = '←';
-    marker.click(function() {
-      return next(0);
-    });
     questionNumber = 0;
     for (j = 0, len = adv_q.length; j < len; j++) {
       question = adv_q[j];
