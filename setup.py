7210927a96eddf31582faf5dab811fdb1e6695e7
Zlati Gardev <zlatigardev@zlatis-mbp.raleigh.ibm.com>
2016-05-10 14:27:24 -0400

    Clean up

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name='trycedocker-tutorial',
-    version='0.2.1',
+    name='wsk_tutorial',
+    version='0.1.1',
     author=u'IBM jStart',
     author_email='jstart@us.ibm.com',
     packages=find_packages(),
-    url='https://hub.jazz.net/git/joshisa/trycedocker-tutorial/',
     license='TBD',
-    description='An interactive learning environment to get familiar with the IBM Containers Extension cli',
+    description='An interactive learning environment to get familiar with the OpenWhisk cli',
     long_description=open('README.md').read(),
     include_package_data=True,
 )
